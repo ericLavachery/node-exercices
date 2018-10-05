92eb8d1b1939f90a3e9a30aca03e537eff799add
ericLavachery <eric.lavachery@gmail.com>
2018-10-05 14:40:34 +0200

    server

--- express/app.js
+++ express/app.js
@@ -1,46 +1,21 @@
-var express = require('express');
-var session = require('cookie-session'); // Charge le middleware de sessions
-var bodyParser = require('body-parser'); // Charge le middleware de gestion des paramètres
-var urlencodedParser = bodyParser.urlencoded({ extended: false });
+var http = require('http');
+var fs = require('fs');
 
-var app = express();
+// Chargement du fichier index.html affiché au client
+var server = http.createServer(function(req, res) {
+    fs.readFile('./index.html', 'utf-8', function(error, content) {
+        res.writeHead(200, {"Content-Type": "text/html"});
+        res.end(content);
+    });
+});
 
-/* On utilise les sessions */
-app.use(session({secret: 'todotopsecret'}))
+// Chargement de socket.io
+var io = require('socket.io').listen(server);
 
-/* S'il n'y a pas de todolist dans la session,
-on en crée une vide sous forme d'array avant la suite */
-app.use(function(req, res, next){
-    if (typeof(req.session.todolist) == 'undefined') {
-        req.session.todolist = [];
-    }
-    next();
-})
+// Quand un client se connecte, on le note dans la console
+io.sockets.on('connection', function (socket) {
+    console.log('Un client est connecté !');
+});
 
-/* On affiche la todolist et le formulaire */
-app.get('/todo', function(req, res) {
-    res.render('todo.ejs', {todolist: req.session.todolist});
-})
 
-/* On ajoute un élément à la todolist */
-app.post('/todo/ajouter/', urlencodedParser, function(req, res) {
-    if (req.body.newtodo != '') {
-        req.session.todolist.push(req.body.newtodo);
-    }
-    res.redirect('/todo');
-})
-
-/* Supprime un élément de la todolist */
-app.get('/todo/supprimer/:id', function(req, res) {
-    if (req.params.id != '') {
-        req.session.todolist.splice(req.params.id, 1);
-    }
-    res.redirect('/todo');
-})
-
-/* On redirige vers la todolist si la page demandée n'est pas trouvée */
-app.use(function(req, res, next){
-    res.redirect('/todo');
-})
-
-app.listen(8080);
+server.listen(8080);

77cbfaf050a9aa36998ba36da5858279c58dfb4a
ericLavachery <eric.lavachery@gmail.com>
2018-10-10 15:45:40 +0200

    serveur retient historique et affiche pour nouveaux arrivants

--- chat/app.js
+++ chat/app.js
@@ -7,6 +7,8 @@ var app = require('express')(),
     io.set('heartbeat timeout', 60000); // Client vers serveur (serveur mort si pas de réponse)
     io.set('heartbeat interval', 25000); // Serveur vers client (client mort si pas de réponse)
 
+var history = [];
+
 // Chargement de la page index.html
 app.get('/', function (req, res) {
     res.sendFile(__dirname + '/index.html');
@@ -18,6 +20,8 @@ io.sockets.on('connection', function (socket, pseudo) {
         pseudo = ent.encode(pseudo);
         socket.pseudo = pseudo;
         socket.broadcast.emit('nouveau_client', pseudo);
+        // console.log(history);
+        socket.emit('give_hist', history);
     });
 
     // On signale aux autres clients quand quelqu'un se déconnecte
@@ -30,6 +34,8 @@ io.sockets.on('connection', function (socket, pseudo) {
     // Dès qu'on reçoit un message, on récupère le pseudo de son auteur et on le transmet aux autres personnes
     socket.on('message', function (message) {
         message = ent.encode(message);
+        data = {pseudo: socket.pseudo, message: message};
+        history.push(data);
         socket.broadcast.emit('message', {pseudo: socket.pseudo, message: message});
     });
 });
